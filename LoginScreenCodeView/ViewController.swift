5a04b146f0a84425dc19a7c2b166e3dfe1f1f3b0
vitor <viktorbruno2006@gmail.com>
2025-04-29 15:30:40 -0300

    Initial Commit

---
+++ LoginScreenCodeView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LoginScreenCodeView
+//
+//  Created by Vítor Bruno on 29/04/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
